c2cfc30c8dc16379cdfb416bd09f7311befc9e74
JustAn <justas.andriusis2001@gmail.com>
2026-07-15 12:52:17 +0300

    SIS-30: wire footer social links to Link entity, note FAQ backend plan
    Add AppExtension Twig function to look up Link URLs by key so
    _footer.html.twig can render facebook/twitter/linkedin icons only when
    a matching admin-managed link exists. Also expand the FAQ section TODO
    with the concrete plan now that feature/SIS-42 defines the Faq entity.

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use App\Repository\LinkRepository;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function __construct(private readonly LinkRepository $linkRepository)
+    {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('link_url', [$this, 'getLinkUrl']),
+        ];
+    }
+
+    public function getLinkUrl(string $key): ?string
+    {
+        return $this->linkRepository->findOneBy(['key' => $key])?->getUrl();
+    }
+}
