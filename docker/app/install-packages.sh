bfe1482181657b63bbf840fc1dcdf0ab05b4b69a
Edvardas Gaidelis <edvardassc@gmail.com>
2026-08-10 15:36:05 +0300

    SIS-68: try it out

--- docker/app/install-packages.sh
+++ docker/app/install-packages.sh
@@ -5,8 +5,9 @@ set -euxo pipefail
 echo $1
 
 if test $1 -eq "1" ; then
-    yarn install
-    APP_ENV=prod php bin/console tailwind:build
     COMPOSER_ALLOW_SUPERUSER=1 APP_ENV=prod composer install --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction
     COMPOSER_ALLOW_SUPERUSER=1 APP_ENV=prod composer dump-autoload --no-dev --classmap-authoritative
+    yarn install
+    APP_ENV=prod php bin/console tailwind:build
+    APP_ENV=prod php bin/console asset-map:compile
 fi
