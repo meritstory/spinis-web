902c660d58e6615942383aa4d2563e1c08aee434
Edvardas Gaidelis <edvardassc@gmail.com>
2026-08-10 15:34:10 +0300

    SIS-68: test out deployment

--- docker/app/install-packages.sh
+++ docker/app/install-packages.sh
@@ -5,8 +5,8 @@ set -euxo pipefail
 echo $1
 
 if test $1 -eq "1" ; then
+    yarn install
+    APP_ENV=prod php bin/console tailwind:build
     COMPOSER_ALLOW_SUPERUSER=1 APP_ENV=prod composer install --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction
     COMPOSER_ALLOW_SUPERUSER=1 APP_ENV=prod composer dump-autoload --no-dev --classmap-authoritative
-    yarn install
-    php bin/console tailwind:build
 fi
